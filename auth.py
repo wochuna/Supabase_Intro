40809d2566223b10c9ab3ee6a105a5485b2ea900
wochuna <wochunayvonne@gmail.com>
2025-10-21 22:55:42 +0300

    Update authentication

--- auth.py
+++ auth.py
@@ -2,25 +2,33 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from supabase import create_client
-from gotrue.exceptions import APIError
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
 
-email: str = "example@gmail.com"
-password: str = "trysupabase1234"
-# user = supabase.auth.sign_up(email=email, password=password) # Sign up a user
 
-'''user = supabase.auth.sign_in(email=email, password=password) # Sign in a user
-print(user)''' # Use session instead
+# Sign up a user
 
-session = None
+'''response = supabase.auth.sign_up(
+    {
+        "email": "wochunayvonne@gmail.com",
+        "password": "trysupabase1234",
+    }
+)'''
 
+# Sign in a user
 try:
-    session = supabase.auth.sign_in(email=email, password=password) # Sign up a user
-except APIError:
-    print("Login failed")
-print(session)
+    response = supabase.auth.sign_in_with_password(
+        "email" ="lynne@gmail.com",
+        "password" ="lynne1",
+        )
+    print("Login successful:", response)
+except Exception as e:
+    print("Login failed:", str(e))
 
-supabase.auth.sign_out() # Close the session
+
+
+# Sign out
+# response = supabase.auth.sign_out()
+
